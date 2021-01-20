8893f6f4130e4600872b125b0c2ee7c0dda7a2f4
JPSO <jpso.guerreiro@gmail.com>
2021-01-20 14:40:26 +0000

    .

--- core.js
+++ core.js
@@ -1,8 +1,9 @@
 const express = require('express')
+var argv = require("minimist")(process.argv)
 const app = express()
 let lib = require("./lib")
 app.use(lib.existent)// the server
 app.use(express.static('.'))
-app.listen(3000, () => {
+app.listen(argv.port || 3000, () => {
   console.log('listening')
 })
