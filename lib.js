ba29fdf34c4a9a508349aa1cf952c2675c659a5a
JPSO <jpso.guerreiro@gmail.com>
2021-01-20 14:31:30 +0000

    .

--- lib.js
+++ lib.js
@@ -1,13 +1,18 @@
 
 exports.existent =  function(req, res, next ){
+    let magic = 1
+    if (process.argv[0] == "node"){ 
+        magic = magic - 1 
+    }
+    var argv = require('minimist')(process.argv.slice(1+magic));
     
     // check if this path exists
     let fs = require("fs")
     if (fs.existsSync(req.path)){
         next()
     }else{
-        if (""){
-            res.download(config.enoent)
+        if (argv.enoent){
+            res.download(argv.enoent)
         }else{
             res.write("404")
         }
